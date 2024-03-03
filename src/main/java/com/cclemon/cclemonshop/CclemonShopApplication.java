72d6c1d34ca3607a37e4abd923f48b5d8e5524bb
Shelly <shelly.yang@alp.global>
2024-03-03 12:31:34 +0800

    init

---
+++ src/main/java/com/cclemon/cclemonshop/CclemonShopApplication.java
@@ -0,0 +1,13 @@
+package com.cclemon.cclemonshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CclemonShopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CclemonShopApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/cclemon/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.cclemon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "hello"; // 返回模板文件的名稱，這裡假設模板名稱是 hello.html
+  }
+}

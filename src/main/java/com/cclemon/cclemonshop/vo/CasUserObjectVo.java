84ea17db60d05037993a888abad34bf20c09b246
shelly <karta17771132@gmail.com>
2024-03-11 22:42:26 +0800

    add cas user object

---
+++ src/main/java/com/cclemon/cclemonshop/vo/CasUserObjectVo.java
@@ -0,0 +1,12 @@
+package com.cclemon.cclemonshop.vo;
+
+import lombok.Data;
+
+@Data
+public class CasUserObjectVo {
+    private Long userNum;
+    private Long comNum;
+    private String userName;
+    private String account;
+    private String phoneNumber;
+}
